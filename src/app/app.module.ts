1112d89d167f38daaa2474086ead82f5ccf0341d
= <luccashiroshi@gmail.com>
2022-06-23 15:25:42 -0300

    table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -21,7 +21,16 @@ import { ProductCreateComponent } from './components/products/product-create/pro
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/products/product-read/product-read.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
@@ -31,7 +40,8 @@ import { MatInputModule } from '@angular/material/input'
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,9 +56,16 @@ import { MatInputModule } from '@angular/material/input'
     HttpClientModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
-  providers: [],
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/products/product.service.ts
+++ src/app/components/products/product.service.ts
@@ -24,4 +24,8 @@ export class ProductService {
   create(product: Product): Observable<Product>{
     return this.http.post<Product>(this.baseURl, product)
   }
+
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseURl)
+  }
 }
